65d9ffe8705c989f5044f05b6ee28b6fc9d7de5c
Jone <764651475@qq.com>
2019-06-03 08:05:29 +0800

    Add led demo

---
+++ IoT_Study/demo_apps/led_blinky/mdk/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'led_blinky' 
+ * Target:  'led_blinky flexspi_nor_release' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define XIP_BOOT_HEADER_ENABLE 1
+#define XIP_BOOT_HEADER_DCD_ENABLE 1
+#define XIP_EXTERNAL_FLASH 1
+
+#endif /* RTE_COMPONENTS_H */
