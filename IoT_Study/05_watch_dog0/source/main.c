27a5538e4ea75127891e7c2d39ecff63a38b2e2d
Jone <764651475@qq.com>
2019-06-14 08:57:07 +0800

    Add wdog1 demo

--- IoT_Study/05_watch_dog0/source/main.c
+++ IoT_Study/05_watch_dog0/source/main.c
@@ -2,6 +2,7 @@
 #include "delay.h"
 #include "uart.h"
 #include "exit.h"
+#include "wdog.h"
 
 int main()
 {
@@ -11,6 +12,8 @@ int main()
 	Lpuart1_Init(115200);
 	Led_Init();				//初始化LED
 	Ext_Init();				//配置按键为外部中断
+	WDOG1_Init(3,2);		//初始化看门狗1,2秒溢出,提前1秒进入中断,方便喂狗
+	
 	
 	while(1)
 	{
