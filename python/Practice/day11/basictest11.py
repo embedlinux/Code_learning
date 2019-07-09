356fddff8aac7d0a0cb1c12c69c8c55a9a06fc33
Jone <764651475@qq.com>
2019-07-09 21:59:25 +0800

    Update func operation test

---
+++ python/Practice/day11/basictest11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Author:
+# @Time    :2019/7/9  21:48
+
+# 1. 函数参数--动态传参
+# 2. 名称空间, 局部名称空间, 全局名称空间, 作⽤用域, 加载顺序.
+# 3. 函数的嵌套
+# 4. gloabal, nonlocal关键字
+
+
+if __name__ == "__main__":
+    pass
