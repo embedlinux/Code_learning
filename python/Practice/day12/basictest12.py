b61a79b4b28a7ea9e87ba0814583cd6aa3dcc133
Jone <qiang.wu@nxp.com>
2019-07-12 13:01:07 +0800

    Update generator test

--- python/Practice/day12/basictest12.py
+++ python/Practice/day12/basictest12.py
@@ -36,19 +36,65 @@ x = {
  [1517992744.94, 350], [1517992800.94, 280]]
 3，求结果：
     v = [i % 2 for i in range(10)]
-    print(v)
+    print(v)            # [0 1 0 1 0 1 ...]
 
 4，求结果:
     v = (i % 2 for i in range(10))
-    print(v)
+    print(v)            # generator
 
 5，求结果：
 
     for i in range(5):
         print(i)
-    print(i)
+    print(i)            # 0 1 2 3 4 4
 '''
 
 
+def ques2():
+    li = ['bn', 'vho', 'bvha', 'f', 'hfiwh', 'eqhrv', 'bvbz', 'vzv']
+    li1 = [i.upper() for i in li if len(i) >= 3]
+    print("(1):{}".format(li1))
+
+    li2 = [(x, y) for x in range(5) if x%2 == 0 for y in range(5) if y%2 != 0]
+    print("(2):{}".format(li2))
+
+    M = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    li3 = [[x, y, z] for x in M[0] if x==3 for y in M[1] if y==6 for z in M[2] if z==9]
+    print("(3):{}".format(li3))
+
+    li4 = [x*x for x in range(50) if x%3 == 0]
+    print("(4):{}".format(li4))
+
+    li5 = ["python" + str(i) + "期" for i in range(1, 11)]
+    print("(5):{}".format(li5))
+
+    li6 = [(x, y) for x in range(6) for y in range(7) if y==x+1]
+    print("(6):{}".format(li6))
+
+    li7 = [x for x in range(20) if x%2 == 0]
+    print("(7):{}".format(li7))
+
+    li = ['alex', 'WuSir', '老男孩', '太白']
+    li8 = [x + str(i) for i, x in enumerate(li)]
+    print("(8):{}".format(li8))
+
+    x = {
+        'name': 'alex',
+        'Values': [{'timestamp': 1517991992.94,
+                    'values': 100, },
+                   {'timestamp': 1517992000.94,
+                    'values': 200, },
+                   {'timestamp': 1517992014.94,
+                    'values': 300, },
+                   {'timestamp': 1517992744.94,
+                    'values': 350},
+                   {'timestamp': 1517992800.94,
+                    'values': 280}
+                   ], }
+    li9 = [[i['timestamp'], i['values']] for i in x['Values']]
+    print("(9):{}".format(li9))
+
+
 if __name__ == "__main__":
-    pass
+    ques2()
+    print("-------------end question2----------------")
