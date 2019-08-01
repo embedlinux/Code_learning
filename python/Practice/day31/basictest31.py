fc5be7c7b93f9c8c5343cd5b6138d1b16c1fab0c
Jone <764651475@qq.com>
2019-08-01 21:04:20 +0800

    network and ftp homework

---
+++ python/Practice/day31/basictest31.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Author:
+# @Time    :2019/7/31  20:54
+
+# 网络基础
+#   端口
+#       IP是找电脑, 端口是找电脑上的程序
+#   OSI 7 层模型
+#     应用层/表现层/会话层         传送层     网络层     数据链接层/物理层
+#     应用层                      传送层     网络层     链接层
+#     HTTP/FTP/SMTP/POP3/NFS/SSH  TCP/UDP   IP/ICMP   LAN:(Etherent, Token Ring)/ARP/WAN(Moden,ISDN,ATM)
+#   三次握手
+#   四次断开
+# https://www.cnblogs.com/wupeiqi/articles/5040823.html
+
+# FTP
+#   进度条
+#   计算文件大小
+#   断点续传
+
+
+if __name__ == "__main__":
+    pass
