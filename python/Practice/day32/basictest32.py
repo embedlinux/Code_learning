541b1cbb4837270149d0de386191e382ba783e5e
Jone <764651475@qq.com>
2019-08-04 22:55:51 +0800

    Add lock threading and procress

--- python/Practice/day32/basictest32.py
+++ python/Practice/day32/basictest32.py
@@ -4,10 +4,20 @@
 # Author:
 # @Time    :2019/8/1  22:28
 
+
+# 操作系统的发展史:https://www.cnblogs.com/Eva-J/articles/8253521.html
+
+
 # 1. 操作系统 / 应用程序
 # 2. 操作中的"并发"
+#     并发:伪, 并发是指资源有限的情况下，两者交替轮流使用资源，比如一段路(单核CPU资源)同时只能过一个人，A走一段后，
+#       让给B，B用完继续给A ，交替使用，目的是提高效率。
+#     并行:真,并行是指两者同时执行，比如赛跑，两个人都在不停的往前跑；
 # 3. 其他语言线程、进程
 # 4. Python中线程和进程（GIL锁）
+#     进程:https://www.cnblogs.com/Eva-J/articles/8253549.html
+#     线程:https://www.cnblogs.com/Eva-J/articles/8306047.html
+#     协程:https://www.cnblogs.com/Eva-J/articles/8324673.html
 # 5. Python线程编写 + 锁
 # 6. 小爬虫
 
