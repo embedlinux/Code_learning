40365da20b11cee27fbc0ec27b482ebe5f7b2d3b
Jone <qiang.wu@nxp.com>
2019-08-20 15:54:44 +0800

    Add

--- python/Python基础/day35/day35.py
+++ python/Python基础/day35/day35.py
@@ -14,6 +14,10 @@ client.setblocking(False)     # 不阻塞
 
 
 # IO多路复用
+#     检测多个socket是否已经发生变化（是否连接成功，是否可读/可写）
+#     socket非阻塞
+
+#   基于事件循环实现的异步非阻塞框架：Twisted
 # 基于IO多路复用 + socket实现并发请求
 
 # 解决并发：单线程+IO不等待
