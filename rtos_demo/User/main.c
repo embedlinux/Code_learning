03378063e6f510db8dbe8fde50c12cee36083be4
Jone <764651475@qq.com>
2019-09-02 21:34:58 +0800

    Add list implement

--- rtos_demo/User/main.c
+++ rtos_demo/User/main.c
@@ -1,6 +1,27 @@
+#include "list.h"
 
+//定义根节点
+struct xLIST List_Test;
+
+//定义节点
+struct xLIST_ITEM item1;
+struct xLIST_ITEM item2;
+struct xLIST_ITEM item3;
 
 int main(void)
 {
-;
+	//根节点初始化
+	vListInitialise(&List_Test);
+	
+	//节点初始化
+	vListInitialiseItem(&item1);
+	vListInitialiseItem(&item2);
+	vListInitialiseItem(&item3);
+	
+	//插入节点
+	vListInsert(&List_Test, &item1);
+	vListInsert(&List_Test, &item2);
+	vListInsert(&List_Test, &item3);
+	
+	
 }
