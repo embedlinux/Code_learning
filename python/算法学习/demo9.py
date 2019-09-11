0c4cad5643066a8e6f032d86cc17e92f43b3b5d9
Jone <764651475@qq.com>
2019-09-11 22:28:16 +0800

    Add double link list

--- python/算法学习/demo9.py
+++ python/算法学习/demo9.py
@@ -5,5 +5,27 @@
 # @Time    :2019/9/11  22:16
 
 
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.pre = None
+        self.next = None
+
+
+class DoubleLinkList(object):
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def add_node(self, value):
+        pass
+
+    def del_node(self, value):
+        pass
+
+    def show_node(self):
+        pass
+
+
 if __name__ == "__main__":
     pass
