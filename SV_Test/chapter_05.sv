00d44da4590b52ca83cf0ef7a1238004e60e1456
Jone <292575901@qq.com>
2020-04-08 17:24:32 +0800

    Update chapter4 code source

--- SV_Test/chapter_05.sv
+++ SV_Test/chapter_05.sv
@@ -175,4 +175,103 @@ program automatic test (arb_if.TEST arbif);
 	end
 endprogram : test
 
+// Example 5-16 Signal synchronization
+program automatic test(bus_if.TB bus);
+	initial begin
+		@bus.cb; 					// Continue on active edge in clocking block
+		repeat (3) @bus.cb; 		// Wait for 3 active edges
+		@bus.cb.grant; 				// Continue on any edge
+		@(posedge bus.cb.grant); 	// Continue on posedge
+		@(negedge bus.cb.grant); 	// Continue on negedge
+		wait (bus.cb.grant==1); 	// Wait for expression , No delay if already true
+		@(posedge bus.cb.grant or negedge bus.reset); 		// Wait for several signals
+	end
+endprogram
+
+// Example 5-17 Synchronous interface sample and module drive
+program test(arb_if.TEST arbif);
+	initial begin
+		$monitor("@%0d: grant=%h", $time, arbif.cb.grant);
+		#50 $display("End of test");
+	end
+endprogram
+module arb(arb_if.DUT arbif);
+	initial begin
+		#7  arbif.grant = 1; // @ 7ns
+		#12 arbif.grant = 2; // @ 12ns
+		#18 arbif.grant = 3; // @ 30ns
+	end
+endmodule
+
+// Example 5-18 Testbench using interface with clocking block
+program automatic test (arb_if.TEST arbif);
+	initial begin
+		arbif.cb.request <= 2'b01;
+		$display("@%0d: Drove req=01", $time);
+		repeat (2) @arbif.cb;
+		if (arbif.cb.grant != 2'b01)
+			$display("@%0d: a1: grant != 2'b01", $time);
+	end
+endprogram : test
+
+// Example 5-19 Interface signal drive
+busif.cb.request <= 1; 			// Nonblocking sync drive
+busif.cb.cmd <= cmd_buf; 		// Nonblocking sync drive
+
+// Example 5-20 Driving a synchronous interface
+program test(arb_if.TEST arbif);
+	initial begin
+		# 2 arbif.cb.request <= 3; // @ 2ns
+		#10 arbif.cb.request <= 2; // @ 12ns
+		#18 arbif.cb.request <= 1; // @ 30ns
+		#50 finish;
+	end
+endprogram
+module arb(arb_if.DUT arbif);
+	initial
+		$monitor("@%0d: req=%h", $time, arbif.request);
+endmodule
+
+// Example 5-21 Interface signal drive
+##2 arbif.cb.request <= 0; 	// Wait 2 cycles then assign
+##3; 						// Illegal - must be used with an assignment
+
+// Example 5-22 Bidirectional signals in a program and interface
+interface master_if (input bit clk);
+	wire [7:0] data; 				// Bidirectional signal
+	clocking cb @(posedge clk);
+		inout data;
+	endclocking
+	modport TEST (clocking cb);
+endinterface
+program test(master_if mif);
+	initial begin
+		mif.cb.data <= 'z; 			// Tri-state the bus
+		@mif.cb;
+		$displayh(mif.cb.data); 	// Read from the bus
+		@mif.cb;
+		mif.cb.data <= 7’h5a; 		// Drive the bus
+		@mif.cb;
+		mif.cb.data <= 'z; 			// Release the bus
+	end
+endprogram
+
+// Example 5-23 Bad clock generator in program block
+program bad_generator (output bit clk, out_sig);
+	bit clk=0, out_sig=0;
+	initial
+		forever #5 clk <= ~clk ;
+	initial
+		forever @(posedge clk)
+		out_sig <= ~out_sig;
+endprogram
+
+// Example 5-24 Good clock generator in module
+module clock_generator (output bit clk);
+	bit clk = 1;
+	always #5 clk = ~clk; // Use blocking assignment
+endmodule
+
+
+
 
