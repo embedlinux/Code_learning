1adceed092d1df3bb01bfe659f7622dc2cd2e06b
Jone <764651475@qq.com>
2020-05-10 19:02:15 +0800

    Add AT510

---
+++ MCU验证/AT421-MN-80001/m3designstart/logical/testbench/testcodes/timer_tests/timer_tests.d
@@ -0,0 +1,10 @@
+.\timer_tests.o: ..\..\..\..\software\common\validation\timer_tests.c
+.\timer_tests.o: ..\..\..\..\software\cmsis\Device\ARM\CM3DS\Include\CM3DS_MPS2.h
+.\timer_tests.o: ..\..\..\..\software\cmsis\CMSIS\Include\core_cm3.h
+.\timer_tests.o: F:\Program Filesx86\MDK\ARM\ARMCC\Bin\..\include\stdint.h
+.\timer_tests.o: ..\..\..\..\software\cmsis\CMSIS\Include\core_cmInstr.h
+.\timer_tests.o: ..\..\..\..\software\cmsis\CMSIS\Include\core_cmFunc.h
+.\timer_tests.o: ..\..\..\..\software\cmsis\Device\ARM\CM3DS\Include\system_CM3DS.h
+.\timer_tests.o: F:\Program Filesx86\MDK\ARM\ARMCC\Bin\..\include\stdio.h
+.\timer_tests.o: ..\..\..\..\software\common\retarget\uart_stdout.h
+.\timer_tests.o: ..\..\testcodes\generic\CM3DS_function.h
