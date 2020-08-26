b04022a80c1f2f54516b5698b87582bb19cbd9b2
Jone <764651475@qq.com>
2020-08-26 23:01:02 +0800

    Update

--- 路科验证/把大象装进冰箱系列-SV实验/sv_labs/solutions/lab6/router_test_top.sv
+++ 路科验证/把大象装进冰箱系列-SV实验/sv_labs/solutions/lab6/router_test_top.sv
@@ -8,13 +8,13 @@ module router_test_top;
   test t(top_io);
   router dut(
     .reset_n	(top_io.reset_n),
-    .clock	(top_io.clock),
-    .din	(top_io.din),
+    .clock		(top_io.clock),
+    .din		(top_io.din),
     .frame_n	(top_io.frame_n),
     .valid_n	(top_io.valid_n),
-    .dout	(top_io.dout),
+    .dout		(top_io.dout),
     .valido_n	(top_io.valido_n),
-    .busy_n	(top_io.busy_n),
+    .busy_n		(top_io.busy_n),
     .frameo_n	(top_io.frameo_n)
   );
 
